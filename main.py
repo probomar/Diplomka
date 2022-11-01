5b0ce8cdb179e9e434eee16e7da685fd9eff8c42
probomar <91134991+probomar@users.noreply.github.com>
2022-11-01 10:32:01 +0100

    koule + plocha 1. krok

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+import pyvista as pv
+import numpy as np
 
+
+pv.global_theme.show_edges = True
+n = 20
+z_range = 10
+
+# condyle1 = pv.ParametricEllipsoid(6, 10, 5)
+# condyle2 = pv.ParametricEllipsoid(5, 7, 5)
+# transform_matrix = np.array([[1, 0, 0, 3], [0, 1, 0, -1], [0, 0, 1, 0], [0, 0, 0, 1]])
+# femur = condyle1.transform(transform_matrix) + condyle2.transform(-transform_matrix)
+# # femur.plot(show_edges=True)
+# tibia = pv.Plane(i_size=30, j_size=30)
+# # tibia.plot(show_edges=True)
+# knee = tibia.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -5], [0, 0, 0, 1]])) + femur
+# knee.plot(show_edges=True)
+
+
+# condyle1 = pv.ParametricEllipsoid(6, 10, 5)
+condyle1 = pv.Sphere(5, center=(0, 0, 5))
+femur = condyle1
+tib = pv.Plane(i_size=30, j_size=30)
+tibia = tib.triangulate()
+# tibia.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -5], [0, 0, 0, 1]]))
+knee = tibia + femur
+# knee.plot(show_edges=True)
+print(tibia.bounds)
+
+axes = pv.Axes(show_actor=True, actor_scale=15.0, line_width=5)
+view = pv.Plotter()
+view.add_actor(axes.actor)
+view.add_mesh(knee, color='yellow')
+intersection = np.ones(n)*5
+# print(intersection)
+
+flex = pv.Plotter()
+flex.add_mesh(femur)
+flex_axes = pv.Axes(show_actor=True, actor_scale=10.0, line_width=5)
+axes.origin = (0, 0, -5.0)
+flex.add_actor(flex_axes.actor)
+for z in range(z_range):
+    flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0.05], [0, 0, 0, 1]])) + femur
+    flex = femur.rotate_x(1, point=axes.origin, inplace=False)
+    view.add_mesh(flex)
+    # print(tibia.boolean_intersection(flex).n_cells)
+    if tibia.boolean_intersection(flex).n_cells == 0:
+        intersection[z] = 0
+    else:
+        intersection[z] = 1
+print(intersection)
+view.show()
+
+
+axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
+axes.origin = (3.0, 3.0, 3.0)
+p.add_actor(axes.actor)
---
+++ main_v1.py
@@ -0,0 +1,40 @@
+import pyvista as pv
+import numpy as np
+
+
+pv.global_theme.show_edges = True
+distance = 0.001
+z_range = 10
+fi_range = 10
+
+condyle1 = pv.Sphere(5, center=(0, 0, 5))
+femur = condyle1
+tib = pv.Plane(i_size=30, j_size=30)
+tibia = tib.triangulate()
+knee = tibia + femur
+
+axes = pv.Axes(show_actor=True, actor_scale=15.0, line_width=5)
+view = pv.Plotter()
+view.add_actor(axes.actor)
+view.add_mesh(knee, color='yellow')
+intersection = np.ones([z_range, fi_range])*5
+# print(intersection)
+
+
+for z in range(z_range):
+    flexp = femur
+    flexp.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, distance], [0, 0, 0, 1]])) + femur
+    flex_axes = pv.Axes(show_actor=True, actor_scale=10.0, line_width=5)
+    flex_axes.origin = (0, 0, -z)
+
+    for fi in range(fi_range):
+        flex = flexp.rotate_x(2 * (fi + 1), point=axes.origin, inplace=False)
+        view.add_mesh(flex)
+        if tibia.boolean_intersection(flex).n_cells == 0:
+            intersection[z, fi] = 0
+        else:
+            intersection[z, fi] = 1
+        flex_axes.origin = (0, 0, z)
+
+print(intersection)
+view.show()
