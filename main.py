18dd535d6ec9890b715178874bada1f0342daf51
probomar <marie.probostova@fs.cvut.cz>
2022-11-08 17:32:10 +0100

    uprava main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 pv.global_theme.show_edges = True
-distance = 0.001
-z_range = 10
-fi_range = 1
+distance = 0.1
+z_range = 100
+fi_range = 10
 
 condyle1 = pv.ParametricEllipsoid(6, 10, 5,  center=(0, 0, 5))
 femur = condyle1
@@ -19,22 +19,32 @@ view.add_actor(axes.actor)
 view.add_mesh(knee, color='yellow')
 intersection = np.ones([z_range, fi_range])*5
 
+flex_p = femur
 flex = femur
 for fi in range(fi_range):
-    flex.rotate_x(2, inplace=False)
+    flex_p.rotate_x(10, inplace=False)
 
     for z in range(z_range):
-        flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z/1-5], [0, 0, 0, 1]]))
-        view.add_mesh(flex)
-        collision, ncol = tibia.collision(flex)
+        flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z/10-5], [0, 0, 0, 1]]))
+        view.add_mesh(flex_p)
+        collision, ncol = tibia.collision(flex_p)
         print(ncol)
-        if tibia.collision(flex) == 0:
-            intersection[z] = 0
+        if ncol == 0:
+            intersection[z, fi] = 0
         else:
-            intersection[z] = 1
-        flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -(z/1-5)], [0, 0, 0, 1]]))
-    if np.where(intersection == 0) > np.where(intersection == 1)
+            intersection[z, fi] = 1
+        # actual_view = tibia + flex
+        # pv.plot(actual_view)
+        flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -(z/10-5)], [0, 0, 0, 1]]))
+
+    if np.where(intersection == 0) > np.where(intersection == 1):
+        z_new = np.where(intersection == 0) -1
+    else:
+        z_new = np.where(intersection == 1)
+
+    flex.rotate_x(10, inplace=False)
+    flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z_new/1-5], [0, 0, 0, 1]]))
 
 print(intersection)
-view.show()
+# view.show()
 
