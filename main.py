3d6496786171e27147f24dccc8e3f400d4e337f0
probomar <marie.probostova@fs.cvut.cz>
2022-11-09 23:42:42 +0100

    main - funkcni otaceni elipsoidu

--- main.py
+++ main.py
@@ -3,48 +3,44 @@ import numpy as np
 
 
 pv.global_theme.show_edges = True
-distance = 0.1
-z_range = 100
-fi_range = 10
+z_range = 80
+z_step = 0.005
+fi_range = 5
+fi_step = 1
 
 condyle1 = pv.ParametricEllipsoid(6, 10, 5,  center=(0, 0, 5))
 femur = condyle1
 tib = pv.Plane(i_size=30, j_size=30)
 tibia = tib.triangulate()
-knee = tibia + femur
-
-axes = pv.Axes(show_actor=True, actor_scale=15.0, line_width=5)
-view = pv.Plotter()
-view.add_actor(axes.actor)
-view.add_mesh(knee, color='yellow')
-intersection = np.ones([z_range, fi_range])*5
 
 flex_p = femur
 flex = femur
 for fi in range(fi_range):
-    flex_p.rotate_x(10, inplace=False)
+    axes = pv.Axes(show_actor=True, actor_scale=15.0, line_width=5)
+    view = pv.Plotter()
+    view.add_actor(axes.actor)
+    view.view_yz()
+    intersection = np.ones([z_range]) * 5
+    flex_p.rotate_x(fi_step, inplace=True)
+    flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -0.25], [0, 0, 0, 1]]))
 
     for z in range(z_range):
-        flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z/10-5], [0, 0, 0, 1]]))
-        view.add_mesh(flex_p)
+        flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z_step], [0, 0, 0, 1]]))
         collision, ncol = tibia.collision(flex_p)
-        print(ncol)
-        if ncol == 0:
-            intersection[z, fi] = 0
-        else:
-            intersection[z, fi] = 1
-        # actual_view = tibia + flex
-        # pv.plot(actual_view)
-        flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -(z/10-5)], [0, 0, 0, 1]]))
+        intersection[z] = ncol
 
-    if np.where(intersection == 0) > np.where(intersection == 1):
-        z_new = np.where(intersection == 0) -1
+    print(intersection)
+    flex_p.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -0.5], [0, 0, 0, 1]]))
+
+    a = np.where(intersection == 0)
+    b = np.where(intersection > 0)
+
+    if a[0][0] > b[0][0]:
+        z_new = a[0][0] - 1
     else:
-        z_new = np.where(intersection == 1)
+        z_new = b[0][0]
 
-    flex.rotate_x(10, inplace=False)
-    flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z_new/1-5], [0, 0, 0, 1]]))
-
-print(intersection)
-# view.show()
-
+    flex.transform(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z_new * z_step], [0, 0, 0, 1]]))
+    actual_view = tibia + flex
+    view.add_mesh(actual_view)
+    view.show()
